5ef2125ed8bb3d2b60b90126e298bfbcd3590ab7
Felipe Gené <felipegenef@gmail.com>
2022-03-08 00:31:29 -0300

    first setup

---
+++ lib/app/Custom/Menu.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:my_pendrive_wallet_desktop/app/Custom/Menu/code.dart';
+import 'package:my_pendrive_wallet_desktop/app/constants.dart';
+
+class MenuBubble extends StatefulWidget {
+  const MenuBubble({Key key}) : super(key: key);
+
+  @override
+  State<MenuBubble> createState() => _MenuBubbleState();
+}
+
+class _MenuBubbleState extends State<MenuBubble>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _animation;
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 260),
+    );
+
+    final curvedAnimation =
+        CurvedAnimation(curve: Curves.easeInOut, parent: _animationController);
+    _animation = Tween<double>(begin: 0, end: 1).animate(curvedAnimation);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionBubble(
+      // Menu items
+      items: <Bubble>[
+        // Floating action menu item
+        Bubble(
+          title: "Export",
+          bubbleColor: DARK_BLUE,
+          icon: Container(
+            margin: const EdgeInsets.only(right: 30),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 15),
+                  color: Colors.black.withOpacity(0.6),
+                  spreadRadius: 0,
+                  blurRadius: 15,
+                ),
+              ],
+            ),
+            child: Image(
+              height: 40,
+              image: AssetImage("assets/export.png"),
+            ),
+          ),
+          titleStyle: TextStyle(fontSize: 16, color: Colors.white),
+          onPress: () {
+            _animationController.reverse();
+          },
+        ),
+        // Floating action menu item
+        Bubble(
+          title: "Help",
+          bubbleColor: DARK_BLUE,
+          icon: Container(
+            margin: const EdgeInsets.only(right: 30),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 15),
+                  color: Colors.black.withOpacity(0.6),
+                  spreadRadius: 0,
+                  blurRadius: 15,
+                ),
+              ],
+            ),
+            child: Image(
+              height: 40,
+              image: AssetImage("assets/help.png"),
+            ),
+          ),
+          titleStyle: TextStyle(fontSize: 16, color: Colors.white),
+          onPress: () {
+            _animationController.reverse();
+          },
+        ),
+        //Floating action menu item
+        Bubble(
+          title: "Back",
+          bubbleColor: DARK_BLUE,
+          icon: Container(
+            margin: const EdgeInsets.only(right: 30),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 15),
+                  color: Colors.black.withOpacity(0.6),
+                  spreadRadius: 0,
+                  blurRadius: 15,
+                ),
+              ],
+            ),
+            child: Image(
+              height: 40,
+              image: AssetImage("assets/arrow_back.png"),
+            ),
+          ),
+          titleStyle: TextStyle(fontSize: 16, color: Colors.white),
+          onPress: () {
+            _animationController.reverse();
+          },
+        ),
+      ],
+
+      // animation controller
+      animation: _animation,
+
+      // On pressed change animation state
+      onPress: () => _animationController.isCompleted
+          ? _animationController.reverse()
+          : _animationController.forward(),
+
+      // Floating Action button Icon color
+      iconColor: Colors.blue,
+
+      // Flaoting Action button Icon
+      iconData: Image(
+        image: AssetImage("assets/menu.png"),
+      ),
+      backGroundColor: Colors.white,
+    );
+  }
+}
---
+++ lib/app/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+
+const DARK_BLUE = Color(0xff75ABFB);
+const LIGHT_BLUE = Color(0xff84E9FC);
+const WHITE = Color(0xffFBF9F9);
+const BITCOIN_COLOR = Color(0xfffbbf24);
+const ETHEREUM_COLOR = Color(0xff9333ea);
+const LITECOIN_COLOR = Color(0xffc026d3);
+const TRANSPARENT = Color.fromRGBO(226, 232, 240, 0.3);
+const BACKGROUND = Color(0xff1e293b);
+const BITCOIN_GRADIENT_COLOR = [
+  BITCOIN_COLOR,
+  Color(0xfffb923c),
+  BITCOIN_COLOR,
+  Color(0xfffb923c)
+];
+const ETHEREUM_GRADIENT_COLOR = [
+  Color(0xff4338ca),
+  ETHEREUM_COLOR,
+  Color(0xff4338ca),
+  ETHEREUM_COLOR,
+];
+const LITECOIN_GRADIENT_COLOR = [
+  Color(0xffe879f9),
+  LITECOIN_COLOR,
+  Color(0xffe879f9),
+  LITECOIN_COLOR,
+];
